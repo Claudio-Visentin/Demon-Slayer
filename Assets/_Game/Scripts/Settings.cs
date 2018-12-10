d3bbe4dcea70aa420696abc4eb01ee3bac268032
Claudio-Visentin <D16126698@mydit.ie>
2018-12-10 13:52:50 +0000

    First Commit

---
+++ Assets/_Game/Scripts/Settings.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour {
+
+    Scene m_scene;
+    public static bool gamepad;
+
+	void Start () {
+        Screen.SetResolution(640, 480, true);
+        Application.targetFrameRate = 60;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        m_scene = SceneManager.GetActiveScene();
+    }
+    public void Gamepad(bool value)
+    {
+        if (value == true)
+        {
+            gamepad = true;
+        }
+        else
+        {
+            gamepad = false;
+        }
+    }
+        void Update () {
+		if (Input.GetKey (KeyCode.Escape) | Input.GetKey (KeyCode.Joystick1Button7))
+        {
+            if (m_scene.name == "MainMenu")
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+	}
+}
---
+++ Assets/_Game/Scripts/SwordScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour {
+    public BoxCollider trigger;
+    public List <GameObject> zombies = new List<GameObject> ();
+    public bool animationtrigger;
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag ("Enemy"))
+        {
+            zombies.Add (col.gameObject);
+            foreach  (GameObject zombie in zombies)
+            {
+                zombie.GetComponent <ZombieAI>().isinRange = true;
+            }
+            for (int i = 0; i < zombies.Count; i++)
+            {
+                if (zombies[i] == null)
+                {
+                    zombies.Remove(zombies[i]);
+                }
+            }
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            zombies.Remove(col.gameObject);
+            foreach (GameObject zombie in zombies)
+            {
+                zombie.GetComponent<ZombieAI>().isinRange = false;
+            }
+        }
+    }
+    void isAttacking()
+    {
+        for (int i = 0; i < zombies.Count; i++)
+        {
+            if (zombies [i] == null)
+            {
+                zombies.Remove(zombies[i]);
+            }
+        }
+        foreach (GameObject zombie in zombies)
+        {
+            animationtrigger = true;
+            zombie.GetComponent<ZombieAI>().health -= 1;
+            zombies.Remove(zombie);
+            
+        } 
+        
+    }
+}
